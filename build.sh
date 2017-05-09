a33920043b44f9b34150636e097e17b0404273b8
jollepar <llergo.jo@gmail.com>
2017-05-09 14:56:00 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -20,7 +20,6 @@ cp -rf build/numpy/lib64/python2.7/site-packages/numpy lambda-package
 		-D CMAKE_BUILD_TYPE=RELEASE				\
 		-D WITH_TBB=ON							\
 		-D WITH_IPP=ON							\
-		-D WITH_FFMPEG=ON						\
 		-D WITH_V4L=ON							\
 		-D ENABLE_AVX=ON						\
 		-D ENABLE_SSSE3=ON						\
